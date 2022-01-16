e20a04ad90336ccb3bf3c77815a2f8ee797a405d
MattSharp05 <45979221+MattSharp05@users.noreply.github.com>
2022-01-16 17:09:28 -0500

    Update

---
+++ CommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Matthew Sharp on 2022/01/16.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
